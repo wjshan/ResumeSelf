43ccac4570128d26dbc51a52e8cb0eb49653cf18
shanweijia <454273687@qq.com>
2023-02-11 16:07:28 +0800

    fix(): change base config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import markdownItKatex from '@iktakahiro/markdown-it-katex'
 import markdownToResume from './core/vite-plugin';
 
 export default defineConfig({
-  base: '/resume',
+  base: './',
   plugins: [
     markdownToResume({
       markdown(md) {
