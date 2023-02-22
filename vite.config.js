fc893e411fc5a623ac3f34f3aaec71935fee6d45
shanweijia <454273687@qq.com>
2023-02-22 13:40:29 +0800

    fix(): add table css

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
-import { defineConfig } from 'vite';
-import markdownItImsize from 'markdown-it-imsize';
-import markdownItKatex from '@iktakahiro/markdown-it-katex'
-import markdownToResume from './core/vite-plugin';
+import { defineConfig } from "vite";
+import markdownItImsize from "markdown-it-imsize";
+import markdownItKatex from "@iktakahiro/markdown-it-katex";
+import markdownToResume from "./core/vite-plugin";
+// import markdownItMultimdTable from "markdown-it-multimd-table";
 
 export default defineConfig({
-  base: './',
+  base: "./",
   plugins: [
     markdownToResume({
       markdown(md) {
         md.use(markdownItImsize);
         md.use(markdownItKatex);
+        // md.use(markdownItMultimdTable,{
+        //   multiline:  false,
+        //   rowspan:    false,
+        //   headerless: false,
+        //   multibody:  true,
+        //   aotolabel:  true,
+        // });
       },
-      pdfName: '个人简历',
+      pdfName: "个人简历",
       pdfMargin: 0,
-      webTitle: '个人简历 - markdown-resume-self'
-    })
+      webTitle: "个人简历 - markdown-resume-self",
+    }),
   ],
   build: {
-    assetsDir: './'
-  }
+    assetsDir: "./",
+  },
 });
